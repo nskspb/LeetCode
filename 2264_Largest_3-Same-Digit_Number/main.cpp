609cd679b912d5cddea6cd534067aebe039bb0dc
Ruslan <nugaev.ruslan2010@yandex.ru>
2023-12-04 21:48:01 +0300

    Accepted

--- 2264_Largest_3-Same-Digit_Number/main.cpp
+++ 2264_Largest_3-Same-Digit_Number/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 
-using namespace std;
-
-string largestGoodInteger(string num)
+std::string largestGoodInteger(std::string num)
 {
-    char *phr;
-    phr = std::strstr(num, "999");
+    const char *phr = NULL;
+    const char *str2[10] = {"999", "888", "777", "666", "555", "444", "333", "222", "111", "000"};
+    for (auto i = 0; i < 10; ++i)
+    {
+        phr = std::strstr(num.c_str(), str2[i]);
+        if (phr)
+        {
+            return str2[i];
+        }
+    }
+    phr = "";
     return phr;
 }
 
 int main()
 {
-    std::cout << largestGoodInteger("12321546999");
+    std::cout << largestGoodInteger("1232333154999655888");
     return 0;
 }
