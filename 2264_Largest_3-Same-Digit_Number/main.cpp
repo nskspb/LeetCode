4a882831ab1073b5927c68ed52277f7078f510a7
Ruslan <nugaev.ruslan2010@yandex.ru>
2023-12-04 20:57:56 +0300

    2264 add

---
+++ 2264_Largest_3-Same-Digit_Number/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string largestGoodInteger(string num)
+{
+    return num;
+}
+
+int main()
+{
+    std::cout << largestGoodInteger("123213");
+    return 0;
+}
